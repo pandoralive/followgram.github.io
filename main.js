78f94ab4f8710c08bca8b5da6b91ddf8c709c161
Christian S'Domingo <161882566+pandoralive@users.noreply.github.com>
2024-04-10 23:55:55 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function redireccionar_home() {
+  window.location.href = 'index.html';
+}
+
+function redireccionar_about() {
+  window.location.href = 'index.html';
+}
+
+function redireccionar_gallery() {
+  window.location.href = 'index.html';
+}
+
+function redireccionar_download() {
+  window.location.href = 'index.html';
+}
+
+function redireccionar_contact() {
+  window.location.href = '/index.html';
+}
+
+function redireccionar_whatsapp() {
+  window.location.href = 'https://api.whatsapp.com/send?phone=5195508107&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202.';
+}
+
+function redireccionar_telegram() {
+  window.location.href = 'https://t.me/welcome2RHK';
+}
+
+function redireccionar_youtube() {
+  window.location.href = 'https://www.youtube.com/';
+}
+
+function redireccionar_tiktok() {
+  window.location.href = 'https://t.me/welcome2RHK';
+}
+
+function redireccionar_github() {
+  window.location.href = 'https://t.me/welcome2RHK';
+}
+
